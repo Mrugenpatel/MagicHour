3dffe43e74300ec661864ef960f6aa107dc6e015
sventouz <sventouz@gmail.com>
2020-01-26 12:21:52 +0900

    ローカル通知作成

--- MagicHour1/AppDelegate.swift
+++ MagicHour1/AppDelegate.swift
@@ -7,61 +7,34 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
-   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-            let center = UNUserNotificationCenter.current()
-            center.requestAuthorization(options: [.alert, .badge, .sound]) {(granted, error) in
-                if granted {
-                    print("許可する")
-                } else {
-                    print("許可しない")
-                }
-            }
-            setLocalNotification(title:"今日は納豆の日です", message:"oh! natto!で納豆を混ぜてみませんか？",month: 1, day: 22)
-            return true
-        }
-
-    private func setLocalNotification(title:String = "", message:String, month: Int, day: Int,hour:Int = 22, minute:Int = 4, second:Int = 0 ){
-            // タイトル、本文、サウンド設定の保持
-            let content = UNMutableNotificationContent()
-            content.title = title
-            content.body = message
-            content.sound = UNNotificationSound.default
-            
-            var notificationTime = DateComponents()
-            notificationTime.month = month
-            notificationTime.day = day
-            notificationTime.hour = hour
-            notificationTime.minute = minute
-            notificationTime.second = second
-            
-            let trigger: UNNotificationTrigger = UNCalendarNotificationTrigger(dateMatching: notificationTime, repeats: false)
-            
-            // 識別子とともに通知の表示内容とトリガーをrequestに内包
-            let request = UNNotificationRequest(identifier: "Natto", content: content, trigger: trigger)
-            
-            // UNUserNotificationCenterにrequestを加える
-            let center = UNUserNotificationCenter.current()
-            center.delegate = self
-            center.add(request) { (error) in
-                if let error = error {
-                    print(error.localizedDescription)
-                }
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // 通知許可の取得
+        UNUserNotificationCenter.current().requestAuthorization(
+        options: [.alert, .sound, .badge]){
+            (granted, _) in
+            if granted{
+                UNUserNotificationCenter.current().delegate = self
             }
         }
-
-
+        return true
+    }
 }
 
 extension AppDelegate: UNUserNotificationCenterDelegate{
+    
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         // アプリ起動中でもアラートと音で通知
         completionHandler([.alert, .sound])
     }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
 }
+
--- MagicHour1/ViewController.swift
+++ MagicHour1/ViewController.swift
@@ -11,15 +11,14 @@ import Alamofire
 import SwiftyJSON
 import NVActivityIndicatorView
 import CoreLocation
-
+import UserNotifications
 class ViewController: UIViewController, CLLocationManagerDelegate {
-    
     @IBOutlet weak var locationLabel: UILabel!
-    @IBOutlet weak var conditionLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var sunsetLabel: UILabel!
     @IBOutlet weak var canSeeSunsetLabel: UILabel!
-    
+    @IBOutlet weak var conditionImage: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
     let apiKey = "e846088f82debcd7f279c5792dceb51b"
     var lat = 26.8205
     var lon = 30.8024
@@ -27,17 +26,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     var activityIndicator: NVActivityIndicatorView!
     // user location
     let locationManager = CLLocationManager()
-    
-    
+    let dateFormatter = DateFormatter()
+    // after load
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         let indicatorSize: CGFloat = 70
         let indicatorFrame = CGRect(x: (view.frame.width-indicatorSize)/2, y: (view.frame.height-indicatorSize)/2, width: indicatorSize, height: indicatorSize)
         activityIndicator = NVActivityIndicatorView(frame: indicatorFrame, type: .lineScale, color: UIColor.white, padding: 20.0)
         activityIndicator.backgroundColor = UIColor.black
         view.addSubview(activityIndicator)
-        
         // use popup to check and get location
         locationManager.requestWhenInUseAuthorization()
         if (CLLocationManager.locationServicesEnabled()) {
@@ -45,32 +42,32 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             locationManager.desiredAccuracy = kCLLocationAccuracyBest
             locationManager.startUpdatingLocation()
         }
+        //
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.timeStyle = .short
+        dateFormatter.dateStyle = .short
+        dateFormatter.locale = Locale(identifier: "ja_JP")
+        //date
+        let date = Date()
+        let dateName = date.weekday
+        dateLabel.text = dateName
     }
-    
-    
+    //About weather API
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations[0]
-        print(location)
         lat = location.coordinate.latitude
         lon = location.coordinate.longitude
-        
         Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(apiKey)&units=metric").responseJSON {
-            
             response in
-            
             self.activityIndicator.stopAnimating()
             if let responseStr = response.result.value {
                 let jsonResponse = JSON(responseStr)
                 let jsonWeather = jsonResponse["weather"].array![0]
                 let jsonTemp = jsonResponse["main"]
+                let jsonIcon = jsonWeather["icon"].stringValue
                 let jsonSunset = jsonResponse["sys"]["sunset"].stringValue
-                // whether you can see the sunset or not
-                let jsonCanSeeSunset = jsonWeather["main"]
-                if jsonCanSeeSunset == "Clear" || jsonCanSeeSunset == "Sunny" {
-                    self.canSeeSunsetLabel.text = "Yes"
-                } else {
-                    self.canSeeSunsetLabel.text = "Nope"
-                }
+                //image change
+                self.conditionImage.image = UIImage(named: jsonIcon)
                 //about sunset(converting from date to string)
                 let dt = jsonSunset
                 if let unixTime1 = TimeInterval(dt) {
@@ -81,12 +78,56 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 }
                 //other labels
                 self.locationLabel.text = jsonResponse["name"].stringValue
-                self.conditionLabel.text = jsonWeather["main"].stringValue
                 self.temperatureLabel.text = "\(Int(round(jsonTemp["temp"].doubleValue)))"
+                
+                // whether you can see the sunset or not
+                //date
+                let notificationTime = DateComponents()
+                let hour = notificationTime.hour
+                let minute = notificationTime.minute
+                let jsonCanSeeSunset = jsonWeather["main"]
+                if jsonCanSeeSunset == "Clear" || jsonCanSeeSunset == "Sunny" {
+                    self.canSeeSunsetLabel.text = "Yes"
+                    if hour == 14 && minute == 00 {
+                        self.setLocalNotification(title:"MagicHourです！", message:"本日は天気が良いのでいい夕日が観れるかもしれません！")
+                    }
+                } else {
+                   self.canSeeSunsetLabel.text = "Nope"
+               }
+            }
+        }
+    }
+    //local notification
+    func setLocalNotification(title:String = "", message:String, hour:Int = 14, minute:Int = 0, second:Int = 0 ){
+        // title, message, sound
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = message
+        content.sound = UNNotificationSound.default
+        //date
+        var notificationTime = DateComponents()
+        notificationTime.hour = hour
+        notificationTime.minute = minute
+        notificationTime.second = second
+        let trigger: UNNotificationTrigger = UNCalendarNotificationTrigger(dateMatching: notificationTime, repeats: false)
+        //identifier
+        let request = UNNotificationRequest(identifier: "MagicHour", content: content, trigger: trigger)
+        // register local notification
+        UNUserNotificationCenter.current().add(request){ (error : Error?) in
+            if let error = error {
+                print(error.localizedDescription)
             }
         }
     }
 }
 
-
-
+extension Date {
+    var weekday: String {
+        let calendar = Calendar(identifier: .gregorian)
+        let component = calendar.component(.weekday, from: self)
+        let weekday = component - 1
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en")
+        return formatter.weekdaySymbols[weekday]
+    }
+}
